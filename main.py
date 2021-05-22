73e6a7582f2af094e85f7bf286ea92c91093d643
saris <kevin110299@126.com>
2021-05-22 18:17:51 +0800

    B1 第二次递交

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ print("###################################")
 print("Hello")
 print("This operation stands for \'git add .\' ")
 print("###################################")
+
 print("###################################")
 print("Let us make a new branch named B1")
+print("###################################")
+
+print("###################################")
+print("Second time commitment of B1")
 print("###################################")
