2295ce2e3d7e43cae2864eba6660cfab7ebbf650
saris <kevin110299@126.com>
2021-05-22 18:19:35 +0800

    Master 第二次递交

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print("###################################")
 print("Hello")
 print("This operation stands for \'git add .\' ")
 print("###################################")
+
+print("###################################")
+print("Second time commitment of Master")
+print("###################################")
